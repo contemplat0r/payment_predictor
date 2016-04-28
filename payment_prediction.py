4086d3741b66e9da36226fd40db643164f003188
contemplat0r <uldo.forme@gmail.com>
2016-04-28 20:57:27 +0300

    Added automatic parameters selection by GridSearchCV

--- payment_prediction.py
+++ payment_prediction.py
@@ -57,8 +57,6 @@ real_x = scaler.fit_transform(real_x)
 real_y = payment_data_matrix[0:, 5]
 
 
-print 'Score on real data', payment_mlp.score(real_x, real_y)
-
 best_mlp = payment_mlp
 
 for i, (train_indices, test_indices) in enumerate(cross_validation.KFold(depended_var_values.size, n_folds=3, shuffle=True)):
@@ -69,7 +67,6 @@ for i, (train_indices, test_indices) in enumerate(cross_validation.KFold(depende
     train_x = scaler.fit_transform(train_x)
     test_x = scaler.fit_transform(test_x)
     payment_mlp = train_regression_predictor(train_x, train_y, learning_rule='sgd', learning_rate=0.0002, n_iter=20, units=4)
-    pickle.dump(payment_mlp, open('payment_mlp_cv_0%s_sgd.pkl' % str(i), 'wb'))
     crossvalidation_test_score =  payment_mlp.score(test_x, test_y)
 
     if crossvalidation_test_score > score:
@@ -85,7 +82,39 @@ predicted_y = best_mlp.predict(real_x)
 predicted_y = predicted_y.reshape(predicted_y.shape[0],)
 
 
-predicted_and_true_values = DataFrame({'predicted' : list(predicted_y.round(decimals=2)), 'real' : list(real_y)})
+predicted_and_true_values = DataFrame({'predicted' : predicted_y.round(decimals=2), 'real' : real_y})
 print predicted_and_true_values.head(20)
 predicted_and_true_values.to_csv('predicted_and_true_values.csv')
+
+
+# Поэкспериментируем с автоматическим подбором параметров обучаемого 
+# персептрона используя встроенные средства scikit-learn 
+
+units = range(4, 15)
+learning_rate = [0.00002, 0.00001, 0.000005]
+layer_type = ['Rectifier', 'Sigmoid', 'Tanh', 'ExpLin']
+param_grid = {'learning_rate' : learning_rate,
+              'nonlinear__units' : units,
+              'nonlinear__type' : layer_type,
+             }
+nonlinear_layer = Layer('Rectifier', units=4, name='nonlinear')
+tuned_regressor_mlp = Regressor(layers=[nonlinear_layer,
+                                        Layer('Linear')],
+                                learning_rate=0.00002,
+                                n_iter=10)
+x = scaler.fit_transform(independed_vars_values)
+y = depended_var_values
+gs = GridSearchCV(tuned_regressor_mlp, param_grid=param_grid, cv=3)
+gs.fit(x, y)
+print 'gs.bets_params_:', gs.best_params_, 'gs_best_score_: ', gs.best_score_
+best_mlp_by_gs = gs.best_estimator_
+
+pickle.dump(payment_mlp, open('payment_mlp_best_by_grid_search.pkl', 'wb'))
+print 'predicted by best gs mlp values and real values score: ', best_mlp_by_gs.score(real_x, real_y)
+
+predicted_y = best_mlp_by_gs.predict(real_x)
+predicted_y = predicted_y.reshape(predicted_y.shape[0],)
+predicted_and_true_values = DataFrame({'predicted' : predicted_y.round(decimals=2), 'real' : real_y})
+predicted_and_true_values.to_csv('predicted_by_best_gs_mlp_and_true_values.csv')
+print predicted_and_true_values.head(20)
 
